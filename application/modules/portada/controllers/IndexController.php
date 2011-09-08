16094be7626a5d1e5ab49190e49f9d836aae11cc
Carlos Caballero <cijkb.j@gmail.com>
2011-09-08 04:57:19 -0400

    Agregadas cuestiones patrocinisticas

--- application/modules/portada/controllers/IndexController.php
+++ application/modules/portada/controllers/IndexController.php
@@ -4,10 +4,13 @@ class IndexController extends Jornadas_Controllers_Action
 {
     public function indexAction() {
         $now = time();
-        //$tow = mktime(14, 0, 0, 9, 8, 2011);
-        $tow = mktime(2, 25, 0, 9, 7, 2011);
+        $tow = mktime(14, 0, 0, 9, 8, 2011);
 
         $dif = $tow - $now;
+        if ($dif < 0) {
+            $dif = 0;
+        }
+
         $days = floor($dif / (60 * 60 * 24));
 
         $dif -= $days * (60 * 60 * 24);
@@ -24,6 +27,7 @@ class IndexController extends Jornadas_Controllers_Action
         $seconds = str_pad($dif, 2, '0', STR_PAD_LEFT);
 
         $this->view->countdown = "$days:$hours:$minutes:$seconds";
+        $this->view->preview = ($dif <> 0);
     }
 
     public function auspiciadoresAction() {}
--- application/modules/portada/views/scripts/index/propaganda.php
+++ application/modules/portada/views/scripts/index/propaganda.php
@@ -1,13 +1,23 @@
 <?php echo $this->partial('portada/views/scripts/prompt.php', array(
     'pwd' => '~',
-    'cmd' => 'cacaview /afiches/principal.jpg',
+    'cmd' => 'ls -lh /afiches',
     'sudo' => false,
     'user' => $this->user,
     'role' => $this->role,
 )) ?>
 <br />
-<img src="/media/afiches/afiche_oficial.jpg" width="100%" alt="" title="" />
 
+total 3,6M
+</p>
+<table>
+    <tr><td>-rw-r--r--</td><td>1</td><td>guest</td><td>guest</td><td>422K</td><td>2011-09-01 23:29</td><td><a class="bold magenta" href="/media/afiches/afiche.jpg">afiche.jpg</a></td></tr>
+    <tr><td>-rw-r--r--</td><td>1</td><td>guest</td><td>guest</td><td>384K</td><td>2011-09-01 23:29</td><td><a class="bold red" href="/media/afiches/afiche.tar.gz">afiche.tar.gz</a></td></tr>
+    <tr><td>-rw-r--r--</td><td>1</td><td>guest</td><td>guest</td><td>575K</td><td>2011-09-01 23:29</td><td><a class="bold magenta" href="/media/afiches/triptico_exterior.jpg">triptico_exterior.jpg</a></td></tr>
+    <tr><td>-rw-r--r--</td><td>1</td><td>guest</td><td>guest</td><td>868K</td><td>2011-09-01 23:29</td><td><a class="bold magenta" href="/media/afiches/triptico_interior.jpg">triptico_interior.jpg</a></td></tr>
+    <tr><td>-rw-r--r--</td><td>1</td><td>guest</td><td>guest</td><td>1,4M</td><td>2011-09-01 23:29</td><td><a class="bold red" href="/media/afiches/triptico.tar.gz">triptico.tar.gz</a></td></tr>
+</table>
+<br />
+<p>
 <?php echo $this->partial('portada/views/scripts/prompt.php', array(
     'pwd' => '~',
     'cmd' => '_',
