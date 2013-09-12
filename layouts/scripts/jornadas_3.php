9bfd8984c08bbb142b7fd9d0b6bff042347fd2d3
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2013-09-12 11:55:21 -0400

    [JORNADAS] Corrections in template

--- layouts/scripts/jornadas_3.php
+++ layouts/scripts/jornadas_3.php
@@ -10,64 +10,83 @@
     <body>
         <div id="wrapper">
             <header>
-                <h1>3ras JORNADAS NACIONALES DE SEGURIDAD INFORMÁTICA</h1>
+                <h1>III JORNADAS NACIONALES DE SEGURIDAD INFORMÁTICA</h1>
             </header>
-            <header>
-                <h1>3ras JORNADAS NACIONALES DE SEGURIDAD INFORMÁTICA</h1>
-            </header>
+            <nav>
+                <ul>
+                    <li><a href="">Conferencias</a></li>
+                    <li><a href="">Talleres</a></li>
+                    <li><a href="">Auspiciadores</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
             <section>
                 <h1>Conferencias</h1>
                 <div class="clear"></div>
-                <div class="fill"></div>
+                <div class="fill-1"></div>
                 <article>
-                    <h2>Tendencias en informática forense</h2>
-                    <h3>Lic. Yony Richard Montoya Burgos</h3>
-                    <p>Gerente Técnico en TX Bolivia S.R.L.<br />
-                    Docente de Seguridad y Auditoría de Redes<br />
-                    Director de la Carrera de Ing. de Sistemas - UMSS</p>
+                    <div class="wrapper">
+                        <h2>Tendencias en informática forense</h2>
+                        <h3>Lic. Yony Richard Montoya Burgos</h3>
+                        <p>Gerente Técnico en TX Bolivia S.R.L.<br />
+                        Docente de Seguridad y Auditoría de Redes<br />
+                        Director de la Carrera de Ing. de Sistemas - UMSS</p>
+                    </div>
                 </article>
                 <article>
-                    <h2>Seguridad en Drupal</h2>
-                    <h3>Comunidad Drupal</h3>
-                    <p>Security and Developers for Drupal</p>
+                    <div class="wrapper">
+                        <h2>Seguridad en Drupal</h2>
+                        <h3>Comunidad Drupal</h3>
+                        <p>Security and Developers for Drupal</p>
+                    </div>
                 </article>
                 <article>
-                    <h2>El secreto de un programa de computación</h2>
-                    <h3>Mgr. Helga Riffarachi Z.</h3>
-                    <p>Maestría en Administración y Gerencia (España -
-                    Madrid)<br />
-                    Especialidad en Propiedad Intelectual (Suiza - Ginebra)
-                    </p>
+                    <div class="wrapper">
+                        <h2>El secreto de un programa de computación</h2>
+                        <h3>Mgr. Helga Riffarachi Z.</h3>
+                        <p>Maestría en Administración y Gerencia (España -
+                        Madrid)<br />
+                        Especialidad en Propiedad Intelectual (Suiza - Ginebra)
+                        </p>
+                    </div>
                 </article>
                 <article>
-                    <h2>Inyectando código</h2>
-                    <h3>Luis Antonio Rosales Marcó</h3>
-                    <p>Black Hat Speaker<br />
-                    Consultor Senior en Seguridad de la Información CosimTI.
-                    <br />
-                    Lider del Capítulo OWASP en Bolivia.</p>
+                    <div class="wrapper">
+                        <h2>Inyectando código</h2>
+                        <h3>Luis Antonio Rosales Marcó</h3>
+                        <p>Black Hat Speaker<br />
+                        Consultor Senior en Seguridad de la Información CosimTI.
+                        <br />
+                        Lider del Capítulo OWASP en Bolivia.</p>
+                    </div>
                 </article>
                 <article>
-                    <h2>Identi-key autenticación segura</h2>
-                    <h3>Ing. Lorgio Alexander Lazarte Zurita</h3>
-                    <p>Administrador Nacional de Seguridad Informática -
-                    Nuevatel PCS de Bolivia VIVA.<br />
-                    Docente Diplomado Seguridad Informática.</p>
+                    <div class="wrapper">
+                        <h2>Identi-key autenticación segura</h2>
+                        <h3>Ing. Lorgio Alexander Lazarte Zurita</h3>
+                        <p>Administrador Nacional de Seguridad Informática -
+                        Nuevatel PCS de Bolivia VIVA.<br />
+                        Docente Diplomado Seguridad Informática.</p>
+                    </div>
                 </article>
                 <article>
-                    <h2>Browser exploitation</h2>
-                    <h3>Gonzalo Nina Mamani</h3>
-                    <p>Consultor Independiente de Seguridad de la
-                    Informacion.<br />
-                    Fundador de 0xsecurity</p>
+                    <div class="wrapper">
+                        <h2>Browser exploitation</h2>
+                        <h3>Gonzalo Nina Mamani</h3>
+                        <p>Consultor Independiente de Seguridad de la
+                        Informacion.<br />
+                        Fundador de 0xsecurity</p>
+                    </div>
                 </article>
                 <article>
-                    <h2>NMAp for Dummies and Pentesters</h2>
-                    <h3>Ing. Alvaro X. Andrade Sejas</h3>
-                    <p>Docente para Bolivia, Argentina, y Ecuador en
-                    Certified Computer Forensic Expert. <br />
-                    Consultor en varias organizaciones gubernamentales.
-                    (Bolivia, Ecuador, Panamá)</p>
+                    <div class="wrapper">
+                        <h2>NMAp for Dummies and Pentesters</h2>
+                        <h3>Ing. Alvaro X. Andrade Sejas</h3>
+                        <p>Docente para Bolivia, Argentina, y Ecuador en
+                        Certified Computer Forensic Expert. <br />
+                        Consultor en varias organizaciones gubernamentales.
+                        (Bolivia, Ecuador, Panamá)</p>
+                    </div>
                 </article>
                 <div class="fill-2"></div>
                 <div class="clear"></div>
@@ -75,30 +94,50 @@
             <section>
                 <h1>Talleres</h1>
                 <div class="clear"></div>
-                <div class="fill"></div>
+                <div class="fill-1"></div>
                 <article>
-                    <h2>Hackeando redes inalámbricas</h2>
-                    <h3>Christian Lima Saravia</h3>
-                    <p>La presencia cada vez mayor de redes de comunicaciones
-inalámbricas, asi como el uso de equipos portatiles y dispositivos
-móviles, conectados a redes domésticas y corporativas, expone el
-perímetro de las organizaciones a un gran número de ataques contra
-su infraestructura.
-Este taller surge con el objetivo de cubrir conocimientos sobre
-técnicas de ataque y defensa, mediante un enfoque metodologico,
-agil y usable que ayude a realizar con exito un analisis de seguridad,
-mediante el uso de la metodologia OWISAM (Open Wireless Security
-Assessment Methodology).</p>
-                </article>
-                <article class="fill-top">
-                    <h2>Hackeando sitios web</h2>
-                    <h3>Gonzalo Nina Mamani</h3>
-                    <p style="height: 15.06em;">&nbsp;</p>
+                    <div class="wrapper">
+                        <h2>Hackeando redes inalámbricas</h2>
+                        <h3>Christian Lima Saravia</h3>
+                        <p>La presencia cada vez mayor de redes de
+                        comunicaciones inalámbricas, asi como el uso de equipos
+                        portatiles y dispositivos móviles, conectados a redes
+                        domésticas y corporativas, expone el perímetro de las
+                        organizaciones a un gran número de ataques contra su
+                        infraestructura.</p>
+                        <p>Este taller surge con el objetivo de cubrir
+                        conocimientos sobre técnicas de ataque y defensa,
+                        mediante un enfoque metodologico, agil y usable que
+                        ayude a realizar con exito un analisis de seguridad,
+                        mediante el uso de la metodologia OWISAM (Open Wireless
+                        Security Assessment Methodology).</p>
+                    </div>
                 </article>
                 <article>
-                    <h2>Informática forense</h2>
-                    <h3>Rodolfo Negron Manchego</h3>
-                    <p>&nbsp;</p>
+                    <div class="wrapper">
+                        <h2>Hackeando sitios web</h2>
+                        <h3>Gonzalo Nina Mamani</h3>
+                        <p>En este taller, altamente práctico, descubriremos
+                        cuales son las vulnerabilidades que encontramos más a
+                        menudo en una aplicación web, como explotar dichas
+                        vulnerabilidades y las medidas necesarias para
+                        mitigarlos.</p>
+                    </div>
+                </article>
+                <article>
+                    <div class="wrapper">
+                        <h2>Informática forense</h2>
+                        <h3>Rodolfo Negron Manchego</h3>
+                        <p>El propósito de las técnicas de informática forense,
+                        es buscar, preservar, y analizar información en sistemas
+                        de ordenadores para buscar evidencias potenciales de un
+                        delito.</p>
+                        <p>La informática forense es una demanda creciente en la
+                        moderna sociedad de la información. La pericia forense,
+                        asimismo, podrá requerirse en caso de juicio o arbitraje
+                        sino también en fases previas de disputa o en procesos
+                        de negociación, entre otros.</p>
+                    </div>
                 </article>
                 <div class="fill-2"></div>
                 <div class="clear"></div>
@@ -106,27 +145,19 @@ Assessment Methodology).</p>
             <section>
                 <h1>Auspiciadores</h1>
                 <div class="clear"></div>
-                <div class="fill"></div>
-                <article>
+                <article id="sponsors">
                     <ul>
                         <li><a href=""><img src="/media/auspiciadores/jornadas_v3/tekhne.png" /></a></li>
                         <li><a href=""><img src="/media/auspiciadores/jornadas_v3/truextend.png" /></a></li>
                         <li><a href=""><img src="/media/auspiciadores/jornadas_v3/cosimti.png" /></a></li>
-                        <li><a href=""><img src="/media/auspiciadores/jornadas_v3/koalasoft.png" /></a></li>
-                        <li><a href=""><img src="/media/auspiciadores/jornadas_v3/sec.png" /></a></li>
-                    </ul>
-                    <div class="clear"></div>
-                </article>
-                <article>
-                    <ul>
                         <li><a href=""><img src="/media/auspiciadores/jornadas_v3/forward.png" /></a></li>
+                        <li><a href=""><img src="/media/auspiciadores/jornadas_v3/koalasoft.png" /></a></li>
                         <li><a href=""><img src="/media/auspiciadores/jornadas_v3/smartica.png" /></a></li>
                         <li><a href=""><img src="/media/auspiciadores/jornadas_v3/supersoft.png" /></a></li>
                         <li><a href=""><img src="/media/auspiciadores/jornadas_v3/0xsecurity.png" /></a></li>
+                        <li><a href=""><img src="/media/auspiciadores/jornadas_v3/sec.png" /></a></li>
                     </ul>
-                    <div class="clear"></div>
                 </article>
-                <div class="fill-3" style="padding-bottom: 0.687em;"></div>
                 <div class="clear"></div>
             </section>
             <footer>
